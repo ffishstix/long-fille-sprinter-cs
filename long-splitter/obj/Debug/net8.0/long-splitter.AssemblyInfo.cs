d2dae91eb003f6388d1e452c403dabcbf0b93912
ffishstix <ffishstix.69@gmail.com>
2024-10-03 18:04:46 +0100

    maybe dont use

--- long-splitter/obj/Debug/net8.0/long-splitter.AssemblyInfo.cs
+++ long-splitter/obj/Debug/net8.0/long-splitter.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("long-splitter")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+572cc9536ba372531e82082056f4953eddfc9e29")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8d8203159857ed2814f90ceca006d6d08e76e689")]
 [assembly: System.Reflection.AssemblyProductAttribute("long-splitter")]
 [assembly: System.Reflection.AssemblyTitleAttribute("long-splitter")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
